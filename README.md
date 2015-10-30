b81cc7aa4e65af563dacc5db7a8856b23c5df30f
Dan Rammer <hamersaw@gmail.com>
2015-10-29 20:03:57 -0600

    runs with rustp2p library - need work on rustp2p to continue

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-CQDB - Complex Query Database 
+cqdb - Complex Query Database 
 =============================
+
+Overview
+--------
+A distributed database for fuzzy matching string fields of records.
+
+Examples
+--------
+./cqdb -i croeus -t 15605 -l 127.0.0.1 -p 15605
+./cqdb -i crius -t 15606 -l 127.0.0.1 -p 15606 -s 127.0.0.1 -e 15605
+./cqdb -i cronus -t 15607 -l 127.0.0.1 -p 15607 -s 127.0.0.1 -e 15605
+./cqdb -i hyperion -t 15608 -l 127.0.0.1 -p 15608 -s 127.0.0.1 -e 15605
+./cqdb -i lapetus -t 15609 -l 127.0.0.1 -p 15609 -s 127.0.0.1 -e 15605
+./cqdb -i mnemosyne -t 15610 -l 127.0.0.1 -p 15610 -s 127.0.0.1 -e 15605
+./cqdb -i phoebe -t 15611 -l 127.0.0.1 -p 15611 -s 127.0.0.1 -e 15605
+./cqdb -i rhea -t 15612 -l 127.0.0.1 -p 15612 -s 127.0.0.1 -e 15605
+./cqdb -i tethys -t 15613 -l 127.0.0.1 -p 15613 -s 127.0.0.1 -e 15605
+./cqdb -i theia -t 15614 -l 127.0.0.1 -p 15614 -s 127.0.0.1 -e 15605
+./cqdb -i themis -t 15615 -l 127.0.0.1 -p 15615 -s 127.0.0.1 -e 15605
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
+extern crate argparse;
+use argparse::{ArgumentParser,Store};
+
+extern crate rustp2p;
+use rustp2p::event::Event;
+use rustp2p::service::omniscient::OmniscientService;
+
+use std::net::{Ipv4Addr,SocketAddrV4};
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+   let mut id = "World".to_string();
+   let mut token: u64 = 0 as u64;
+   let mut listen_ip: String = "127.0.0.1".to_string();
+   let mut listen_port: u16 = 0 as u16;
+   let mut seed_ip: String = "127.0.0.1".to_string();
+   let mut seed_port: u16 = 0 as u16;
+   {    //solely to limit scope of parser variable
+        let mut parser = ArgumentParser::new();
+        parser.set_description("Start up a gossiping bearbones node");
+        parser.refer(&mut id).add_option(&["-i", "--id"], Store, "ID of node").required();
+        parser.refer(&mut token).add_option(&["-t", "--token"], Store, "Token of node").required();
+        parser.refer(&mut listen_ip).add_option(&["-l", "--listen-ip"], Store, "Ip address to listen on").required();
+        parser.refer(&mut listen_port).add_option(&["-p", "--listen-port"], Store, "Port to listen on").required();
+        parser.refer(&mut seed_ip).add_option(&["-s", "--seed-ip"], Store, "Ip address for seed node");
+        parser.refer(&mut seed_port).add_option(&["-e", "--seed-port"], Store, "Port for seed node");
+        parser.parse_args_or_exit();
+   }
+
+    let listen_ip = match Ipv4Addr::from_str(&listen_ip[..]) {
+        Ok(listen_ip) => listen_ip,
+        Err(_) => panic!("Unable to parse ip '{}'", listen_ip),
+    };
+
+    let listen_addr = SocketAddrV4::new(listen_ip, listen_port);
+
+    //parse seed ip address
+    let mut seed_addr: Option<SocketAddrV4> = None;
+    if seed_port != 0 {
+        let seed_ip = match Ipv4Addr::from_str(&seed_ip[..]) {
+            Ok(seed_ip) => seed_ip,
+            Err(_) => panic!("Unable to parse ip {}", seed_ip),
+        };
+
+        seed_addr = Some(SocketAddrV4::new(seed_ip, seed_port));
+    }
+
+    //create new service handle
+    let service_handle = OmniscientService::new(id, token, listen_addr, seed_addr);
+
+    //start the service handle
+    let (start_handle, rx) = service_handle.start();
+    while let Ok(event) = rx.recv() {
+        match event {
+            Event::JoinMsgEvent(id, token, socket_addr) => {
+                println!("recv JoinMsgEvent({}, {}, {})", id, token , socket_addr);
+            },
+            Event::LookupMsgEvent(token) => {
+                println!("recv LookupMsgEvent({})", token);
+            },
+            Event::GenericMsgEvent(vec, _) => {
+                println!("recv GenericMsgEvent() of length {}", vec.len());
+            }
+            _ => println!("not processing this event type"),
+        }
+    }
+ 
+    start_handle.join().unwrap();
 }
